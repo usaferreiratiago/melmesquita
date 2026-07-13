b840a880f60556cf4492d401f42b2599a3c48075
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-07-13 08:03:37 -0300

    melmesquita

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -211,6 +211,12 @@ function Home() {
               key={c.to}
               to={c.to}
               className="group flex items-start justify-between gap-6 border-t border-zinc-200 dark:border-zinc-800 pt-8 transition hover:border-zinc-900 dark:hover:border-zinc-50"
+              search={function (current: never): never {
+                throw new Error("Function not implemented.");
+              }}
+              params={function (current: never): never {
+                throw new Error("Function not implemented.");
+              }}
             >
               <div>
                 <div className="text-xs tabular-nums tracking-widest text-zinc-500 dark:text-zinc-400">
